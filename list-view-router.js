cf8730f644aaf7c62889f0470cfa6c2337fb6cb9
daniela ramirez alvarez <dani.maramirez@gmail.com>
2023-07-08 17:20:27 -0500

    Se creo un middleware para list-view-router que gestione que los parametros sean correctos

--- list-view-router.js
+++ list-view-router.js
@@ -3,6 +3,26 @@ const router = express.Router();
 
 const listaTareas = require("./listaTareas.json");
 
+const paramMiddleware = (req, res, next) => {
+  const param = req.params.param;
+
+  switch (param) {
+    case "completas":
+    case "incompletas":
+      next();
+      break;
+
+    default:
+      return res.status(400).send({
+        success: false,
+        content: "Parametro invalido",
+      });
+  }
+  next();
+};
+
+router.use("/:param", paramMiddleware);
+
 router.get("/completas", (req, res) => {
   let tareasCompletas = listaTareas.filter(
     (tarea) => tarea.isCompleted === true
