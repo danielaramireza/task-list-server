4a391b3db3998a06ded5ae5c21b01d3b8bd6997d
daniela ramirez alvarez <dani.maramirez@gmail.com>
2023-07-11 18:33:37 -0500

    Se creo la ruta /login para hacer autenticacion de usuarios y se implemento JWT en la misma

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 const port = 3000;
 const listViewRouter = require("./list-view-router");
 const listEditRouter = require("./list-edit-router");
 const listaTareas = require("./listaTareas.json");
+
+const users = [
+  { email: "damaral@gmail.com", name: "Daniela" },
+  { email: "sevepo@gmail.com", name: "Sebastian" },
+  { email: "clapagaos@gmail.com", name: "Claudia" },
+  { email: "luguera@gmail.com", name: "Luciana" },
+  { email: "anferal@gmail.com", name: "Andres" },
+];
 
 app.use(express.json());
 app.use("/tareas", listViewRouter);
@@ -31,6 +40,20 @@ app.get("/", (req, res) => {
     success: true,
     content: listaTareas,
   });
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const user = users.find((user) => user.email === email);
+
+  if (!user) {
+    res.status(401).send({ error: "Invalid user name or password" });
+  } else {
+    const token = jwt.sign(user, process.env.SECRET_KEY, {
+      algorithm: "HS256",
+    });
+    res.json({ token });
+  }
 });
 
 app.listen(port, () => {
